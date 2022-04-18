47980c30f4a59fa09d66c4374b26f4a39c028b2b
diegoru <diego.ruescas@gmail.com>
2022-04-18 20:43:12 -0300

    seção sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -3,10 +3,22 @@ const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
 menuMobile.addEventListener('click', () => {
-    /* Alternando entre 🍔 e X */
+    /* Alternando entre list e X */
     menuMobile.classList.contains("bi-list")
     ? menuMobile.classList.replace("bi-list", "bi-x")
     : menuMobile.classList.replace("bi-x", "bi-list")
     /* Adicionando classe menu-nav-active ao body para aparecer e desaparecer menu */
     body.classList.toggle("menu-nav-active")
+})
+
+/* Fecha o menu ao clicar em algum item e muda o icone para list*/
+
+const navItem = document.querySelectorAll('.nav-item')
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
 })
